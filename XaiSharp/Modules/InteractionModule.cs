4b30592aefb313ab0d929bc559aed4e7b15dd6c1
Zai <74737630+zai-tm@users.noreply.github.com>
2022-09-25 19:45:35 -0600

    add a few commands
    quotes and dice

--- XaiSharp/Modules/InteractionModule.cs
+++ XaiSharp/Modules/InteractionModule.cs
@@ -1,5 +1,6 @@
 ﻿using Discord;
 using Discord.Interactions;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,6 +11,13 @@ namespace XaiSharp.Modules
 {
     public class InteractionModule : InteractionModuleBase<SocketInteractionContext>
     {
+        public class Quote
+        {
+            public string Text;
+            public string Author;
+            public string? Image;
+        }
+
         [SlashCommand("crash", "Crash the bot... maybe")]
         public async Task HandleCrashCommand()
         {
@@ -22,10 +30,39 @@ namespace XaiSharp.Modules
             await ModifyOriginalResponseAsync(m => m.Content = $"{Context.User.Username} is not in the sudoers file. This incident will be reported.");
         }
 
-        [SlashCommand("two", "wow there are 2 commands now")]
-        public async Task HandleTwoCommands()
+        [SlashCommand("dice", "Rolls a dice!")]
+        public async Task HandleDiceCommand(int? sides = null)
         {
-            await RespondAsync("2 (reference to BATTLE FOR DREAM ISALAND)))!!!!!!!!!");
+            Random random = new();
+            var roll = random.Next(1,6);
+            if (sides != null)
+            {
+                roll = random.Next(1, sides.Value);
+            }
+
+            var diceEmbed = new EmbedBuilder()
+                .WithTitle("Dice")
+                .WithColor(0xffffff)
+                .AddField("The dice landed on...", $"**{roll}**");
+            await RespondAsync(embed: diceEmbed.Build());
+        }
+
+        [SlashCommand("quotes", "A random quote that is most likely dumb")]
+        public async Task HandleQuotesCommand()
+        {
+            List<Quote> quotesList = JsonConvert.DeserializeObject<List<Quote>>(File.ReadAllText("JSON/quotes.json"));
+            var quotes = quotesList.ToArray();
+            Random random = new();
+            var quote = random.Next(0, quotes.Length);
+
+            var quoteEmbed = new EmbedBuilder()
+                .WithColor((uint)Math.Floor(random.NextDouble() * (0xffffff + 1)))
+                .WithAuthor(quotes[quote].Author)
+                .WithDescription($"{quotes[quote].Text.Replace("\\n", "\n")}\n\n[Suggest a quote](https://forms.gle/BAZ2rzgYcaDXf9tR7)")
+                .WithImageUrl(quotes[quote].Image)
+                .WithFooter("Inspirational quotes provided by the Progressbar95 community")
+                .WithCurrentTimestamp();
+            await RespondAsync(embed: quoteEmbed.Build());
         }
     }
 }
