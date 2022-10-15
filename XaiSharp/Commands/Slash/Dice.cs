3b20f2f9a5d68668fb70ed97b0b35155d20ea26f
Zai <74737630+zai-tm@users.noreply.github.com>
2022-10-14 22:51:17 -0600

    change dice to long (to allow LONG numbers)

--- XaiSharp/Commands/Slash/Dice.cs
+++ XaiSharp/Commands/Slash/Dice.cs
@@ -7,23 +7,24 @@ namespace XaiSharp.Commands.Slash
     {
 
         [SlashCommand("dice", "Rolls a dice!")]
-        public async Task Handle(int? sides = null)
+        public async Task Handle(long? sides = null)
         {
             Random random = new();
-            var roll = random.Next(1, 6);
+            long roll = random.NextInt64(1, 6);
             if (sides != null)
             {
                 if (sides <= 0)
                     await RespondAsync("You cannot enter 0 or a negative number!", ephemeral: true);
                 else
-                    roll = random.Next(1, sides.Value+1);
+                    roll = random.NextInt64(1, sides.Value+1);
             }
 
             var diceEmbed = new EmbedBuilder()
                 .WithTitle("Dice")
                 .WithColor(0xffffff)
                 .AddField("The dice landed on...", $"**{roll}**");
-            await RespondAsync(embed: diceEmbed.Build());
+            await DeferAsync();
+            await FollowupAsync(embed: diceEmbed.Build());
         }
     }
 }
