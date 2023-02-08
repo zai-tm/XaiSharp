46cde39f79ff92fd2ba663164147bdaa0c4d0019
Zai <74737630+zai-tm@users.noreply.github.com>
2023-02-07 21:12:15 -0700

    Update Program.cs

--- XaiSharp/Program.cs
+++ XaiSharp/Program.cs
@@ -192,8 +192,8 @@ namespace XaiSharp
 
         public string CreateHyperlinks(string input)
         {
-            Regex regex = new(@"https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)");
-            return regex.Replace(input, @"[$0](<$0>)");
+            Regex regex = new(@"(https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*))");
+            return regex.Replace(input, @"[$1](<$1>)");
         }
 
     }
