37ebc2cfc86668f527a036873e64283467d42cd0
Zai <74737630+zai-tm@users.noreply.github.com>
2023-02-08 08:04:18 -0700

    OOPS!

--- XaiSharp/dbinit.sql
+++ XaiSharp/dbinit.sql
@@ -1,7 +1,7 @@
 CREATE TABLE [Tags] (
     [Id] INTEGER NOT NULL PRIMARY KEY,
     [Name] INT NOT NULL,
-    [Content] INT NOT NULL,
+    [Content] TEXT NOT NULL,
     [UserId] UNSIGNED BIG INT NOT NULL,
     [CreatedAt] DATETIME NOT NULL
 );
