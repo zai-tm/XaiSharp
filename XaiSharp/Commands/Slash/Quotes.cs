eccdc0db7a56db0a818e5982468138068f82c398
Zai <74737630+zai-tm@users.noreply.github.com>
2023-04-25 08:04:51 -0600

    now you can eat sunlight

--- XaiSharp/Commands/Slash/Quotes.cs
+++ XaiSharp/Commands/Slash/Quotes.cs
@@ -6,29 +6,51 @@ namespace XaiSharp.Commands.Slash
 {
     public class Quotes : InteractionModuleBase<SocketInteractionContext>
     {
-        public class Quote
-        {
-            public string Text { get; set; }
-            public string Author { get; set; }
-            public string? Image { get; set; }
-        }
 
-        [SlashCommand("quotes", "A random quote that is most likely dumb")]
-        public async Task Handle()
-        {
-            List<Quote> quotesList = JsonConvert.DeserializeObject<List<Quote>>(File.ReadAllText("JSON/quotes.json"));
-            var quotes = quotesList.ToArray();
-            Random random = new();
-            var quote = random.Next(0, quotes.Length);
+        [Group("quotes", "want a not-so-inspirational quote? this command is for you.")]
 
-            var quoteEmbed = new EmbedBuilder()
-                .WithColor(Convert.ToUInt32(Util.CreateMD5Hash(quotes[quote].Text)[..6], 16))
-                .WithAuthor(quotes[quote].Author)
-                .WithDescription(quotes[quote].Text.Replace("\\n", "\n"))
-                .WithImageUrl(quotes[quote].Image)
-                .WithFooter("Inspirational quotes provided by the Progressbar95 community")
-                .WithCurrentTimestamp();
-            await RespondAsync(embed: quoteEmbed.Build());
+        public class QuoteGroup : InteractionModuleBase<SocketInteractionContext>
+        {
+            public class Quote
+            {
+                public string Text { get; set; }
+                public string Author { get; set; }
+                public string? Image { get; set; }
+            }
+
+            [SlashCommand("random", "A random quote")]
+            public async Task HandleRandom()
+            {
+                List<Quote> quotesList = JsonConvert.DeserializeObject<List<Quote>>(File.ReadAllText("JSON/quotes.json"));
+                var quotes = quotesList.ToArray();
+                Random random = new();
+                var quote = random.Next(0, quotes.Length);
+
+                var quoteEmbed = new EmbedBuilder()
+                    .WithColor(Convert.ToUInt32(Util.CreateMD5Hash(quotes[quote].Text)[..6], 16))
+                    .WithAuthor(quotes[quote].Author)
+                    .WithDescription(quotes[quote].Text.Replace("\\n", "\n"))
+                    .WithImageUrl(quotes[quote].Image)
+                    .WithFooter("Inspirational quotes provided by the Progressbar95 community")
+                    .WithCurrentTimestamp();
+                await RespondAsync(embed: quoteEmbed.Build());
+            }
+
+            [SlashCommand("search", "choose a quote")]
+            public async Task HandleSearch(int id)
+            {
+                List<Quote> quotesList = JsonConvert.DeserializeObject<List<Quote>>(File.ReadAllText("JSON/quotes.json"));
+                var quotes = quotesList.ToArray();
+
+                var quoteEmbed = new EmbedBuilder()
+                    .WithColor(Convert.ToUInt32(Util.CreateMD5Hash(quotes[id-1].Text)[..6], 16))
+                    .WithAuthor(quotes[id - 1].Author)
+                    .WithDescription(quotes[id - 1].Text.Replace("\\n", "\n"))
+                    .WithImageUrl(quotes[id - 1].Image)
+                    .WithFooter("Inspirational quotes provided by the Progressbar95 community")
+                    .WithCurrentTimestamp();
+                await RespondAsync(embed: quoteEmbed.Build());
+            }
         }
     }
 }
