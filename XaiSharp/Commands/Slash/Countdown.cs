067fe0808c5a22cb929860fad1388136fbd581b5
Zai <74737630+zai-tm@users.noreply.github.com>
2023-05-03 20:54:27 -0600

    please work

--- XaiSharp/Commands/Slash/Countdown.cs
+++ XaiSharp/Commands/Slash/Countdown.cs
@@ -15,56 +15,57 @@ namespace XaiSharp.Commands.Slash
             public DateTimeOffset Date { get; set; }
         }
 
+        HttpClient client = new();
+
         [SlashCommand("countdown", "Countdown until an event")]
         public async Task Handle()
         {
             try
             {
-                using (var client = new HttpClient())
+                string url = "https://pb95discord.com/event.json";
+                string result = await client.GetStringAsync(url);
+                Event nextEvent = JsonConvert.DeserializeObject<Event>(result);
+
+                EmbedBuilder countdownEmbed = new()
                 {
-                    var nextEventJson = await client.GetStringAsync("https://pb95discord.com/event.json");
-                    Event nextEvent = JsonConvert.DeserializeObject<Event>(nextEventJson);
-
-                    EmbedBuilder countdownEmbed = new()
+                    Title = "Days until " + nextEvent.Name,
+                    Color = Colors.Negative,
+                    //Description = $"{nextEvent.Emoji} {nextEvent.Name} is <t:{nextEvent.Date.ToUnixTimeSeconds()}:R>! {nextEvent.Emoji}",
+                    Footer = new()
                     {
-                        Title = "Days until " + nextEvent.Name,
-                        Color = Colors.Negative,
-                        //Description = $"{nextEvent.Emoji} {nextEvent.Name} is <t:{nextEvent.Date.ToUnixTimeSeconds()}:R>! {nextEvent.Emoji}",
-                        Footer = new()
-                        {
-                            Text = $"Target date: {nextEvent.Date.ToString("MMMM d, yyyy hh:mm:ss tt")} UTC"
-                        }
-                    };
-                    Console.WriteLine(nextEvent.Date);
-
-                    switch (DateTimeOffset.UtcNow)
-                    {
-                        case var _ when DateTimeOffset.UtcNow.Date < nextEvent.Date.Date:
-                            countdownEmbed.Description = $"{nextEvent.Emoji} {nextEvent.Name} is <t:{nextEvent.Date.ToUnixTimeSeconds()}:R>! {nextEvent.Emoji}";
-                            break;
-                        case var _ when DateTimeOffset.UtcNow.Date == nextEvent.Date.Date:
-                            countdownEmbed.Description = $"{nextEvent.Emoji} **{nextEvent.Name.ToUpper()} IS TODAY!!!** {nextEvent.Emoji}";
-                            countdownEmbed.Color = Colors.Positive;
-                            break;
-                        case var _ when DateTime.UtcNow.Date > nextEvent.Date.Date:
-                            countdownEmbed.Description = $"{nextEvent.Emoji} {nextEvent.Name} was <t:{nextEvent.Date.ToUnixTimeSeconds()}:R>! {nextEvent.Emoji}";
-                            countdownEmbed.Color = 0x888888;
-                            break;
+                        Text = $"Target date: {nextEvent.Date.ToString("MMMM d, yyyy hh:mm:ss tt")} UTC"
                     }
+                };
+                Console.WriteLine(nextEvent.Date);
 
-                    if (nextEvent.ImageUrl != null)
-                    {
-                        countdownEmbed.ThumbnailUrl = nextEvent.ImageUrl;
-                    }
-
-                    await RespondAsync(embed: countdownEmbed.Build());
-
+                switch (DateTimeOffset.UtcNow)
+                {
+                    case var _ when DateTimeOffset.UtcNow.Date < nextEvent.Date.Date:
+                        countdownEmbed.Description = $"{nextEvent.Emoji} {nextEvent.Name} is <t:{nextEvent.Date.ToUnixTimeSeconds()}:R>! {nextEvent.Emoji}";
+                        break;
+                    case var _ when DateTimeOffset.UtcNow.Date == nextEvent.Date.Date:
+                        countdownEmbed.Description = $"{nextEvent.Emoji} **{nextEvent.Name.ToUpper()} IS TODAY!!!** {nextEvent.Emoji}";
+                        countdownEmbed.Color = Colors.Positive;
+                        break;
+                    case var _ when DateTime.UtcNow.Date > nextEvent.Date.Date:
+                        countdownEmbed.Description = $"{nextEvent.Emoji} {nextEvent.Name} was <t:{nextEvent.Date.ToUnixTimeSeconds()}:R>! {nextEvent.Emoji}";
+                        countdownEmbed.Color = 0x888888;
+                        break;
                 }
+
+                if (nextEvent.ImageUrl != null)
+                {
+                    countdownEmbed.ThumbnailUrl = nextEvent.ImageUrl;
+                }
+
+                await RespondAsync(embed: countdownEmbed.Build());
+
+            }
             }
             catch (Exception e)
             {
                 Console.WriteLine(e);
             }
-        }
+}
     }
 }
--- XaiSharp/Commands/Slash/Pomelo.cs
+++ XaiSharp/Commands/Slash/Pomelo.cs
@@ -17,37 +17,45 @@ namespace XaiSharp.Commands.Slash
         [SlashCommand("pomelo", "Check if a pomelo name is available")]
         public async Task Handle([Summary("name", "name to check")]string? name = null)
         {
-            string nameFinal = name ?? Context.User.Username;
-
-            string url = "https://pomelo-checker.onrender.com/check?pomelo="+nameFinal;
-            string result = await client.GetStringAsync(url);
-            PomeloResponse data =  JsonConvert.DeserializeObject<PomeloResponse>(result);
-
-            var pomeloEmbed = new EmbedBuilder()
-                .WithFooter("Data comes from https://pomelo-checker.onrender.com");
-
-            switch (data.Status)
+            try
             {
-                case "available":
-                    {
-                        pomeloEmbed.Color = Colors.Positive;
-                        pomeloEmbed.Description = $"✅ **@{nameFinal.ToLower()}** is available!";
-                        break;
-                    }
-                case "available_ratelimit":
-                    {
-                        pomeloEmbed.Color = Colors.Positive;
-                        pomeloEmbed.Description = $"✅ **@{nameFinal.ToLower()}** is available! (Rate limited)";
-                        break;
-                    }
-                case "unavailable":
-                    {
-                        pomeloEmbed.Color = Colors.Negative;
-                        pomeloEmbed.Description = $"❌ **@{nameFinal.ToLower()}** is unavailable. {data.Message}";
-                        break;
-                    }
+                string nameFinal = name ?? Context.User.Username;
+
+                string url = "https://pomelo-checker.onrender.com/check?pomelo=" + nameFinal;
+                Console.WriteLine(url);
+                string result = await client.GetStringAsync(url);
+                PomeloResponse data = JsonConvert.DeserializeObject<PomeloResponse>(result);
+
+                var pomeloEmbed = new EmbedBuilder()
+                    .WithFooter("Data comes from https://pomelo-checker.onrender.com");
+
+                switch (data.Status)
+                {
+                    case "available":
+                        {
+                            pomeloEmbed.Color = Colors.Positive;
+                            pomeloEmbed.Description = $"✅ **@{nameFinal.ToLower()}** is available!";
+                            break;
+                        }
+                    case "available_ratelimit":
+                        {
+                            pomeloEmbed.Color = Colors.Positive;
+                            pomeloEmbed.Description = $"✅ **@{nameFinal.ToLower()}** is available! (Rate limited)";
+                            break;
+                        }
+                    case "unavailable":
+                        {
+                            pomeloEmbed.Color = Colors.Negative;
+                            pomeloEmbed.Description = $"❌ **@{nameFinal.ToLower()}** is unavailable. {data.Message}";
+                            break;
+                        }
+                }
+                await RespondAsync(embed: pomeloEmbed.Build());
             }
-            await RespondAsync(embed: pomeloEmbed.Build());
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
         }
     }
 }
